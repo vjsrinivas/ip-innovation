b3c0f720658df0056a2931e1f0fa84011589808e
vtang144 <wph612@vols.utk.edu>
2020-07-06 16:00:49 -0400

    Added checkboxes to table

--- innovation/src/app/components/admin/admin.component.ts
+++ innovation/src/app/components/admin/admin.component.ts
@@ -8,15 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class AdminComponent implements OnInit {
 
   userData: any = [
-    {name: "Vicki", badge1:"imaage1"},
-    {name: "Vicki", badge1:"image2"},
-    {name: "Vicki", badge1:"image3"},
-    
+    {name: "Vicki Tang", badge1:" ", badge2: " ", badge3: " ", badge4: " ", badge5: " ", badge6: " ", badge7: " ", badge8: " ", badge9: " ", badge10: " "},
+    {name: "Vijay Rajagopal", badge1:" ", badge2: " ", badge3: " ", badge4: " ", badge5: " ", badge6: " ", badge7: " ", badge8: " ", badge9: " ", badge10: " "},
+    {name: "Vishal Aiely", badge1:" ", badge2: " ", badge3: " ", badge4: " ", badge5: " ", badge6: " ", badge7: " ", badge8: " ", badge9: " ", badge10: " "},
+    {name: "Khiliel Bullock", badge1:" ", badge2: " ", badge3: " ", badge4: " ", badge5: " ", badge6: " ", badge7: " ", badge8: " ", badge9: " ", badge10: " "},
+    {name: "Tiara Jefferson", badge1:" ", badge2: " ", badge3: " ", badge4: " ", badge5: " ", badge6: " ", badge7: " ", badge8: " ", badge9: " ", badge10: " "},
+  
   ]
 
   constructor() { 
     console.log(this.userData);
   }
+
+  
 
   ngOnInit() {
   }
