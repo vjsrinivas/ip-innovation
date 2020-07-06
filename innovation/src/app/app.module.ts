f39af6cf0c7830c72946675783db82507184b507
Vishal Aiely <vishalaiely@Vishals-MacBook-Pro.local>
2020-07-06 11:16:57 -0500

    added styling (angular material)

--- innovation/src/app/app.module.ts
+++ innovation/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ExportCsvComponent } from './export-csv/export-csv.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
