f4a60a56c7ddc6e33a0c058a9603bafd15930343
Vishal Aiely <vishalaiely@Vishals-MacBook-Pro.local>
2020-07-06 11:00:05 -0500

    json to csv component completed not styled

--- innovation/src/app/app.module.ts
+++ innovation/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { ExportCsvComponent } from './export-csv/export-csv.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ExportCsvComponent } from './export-csv/export-csv.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- innovation/src/app/export-csv/export-csv.component.ts
+++ innovation/src/app/export-csv/export-csv.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import {saveAS} from 'file-saver'
+import { Component, OnInit, Input } from '@angular/core';
+import {json2csv} from 'json-2-csv'
+import {saveAs} from 'file-saver'
+import { convertPropertyBinding } from '@angular/compiler/src/compiler_util/expression_converter';
 
 @Component({
   selector: 'app-export-csv',
   templateUrl: './export-csv.component.html',
   styleUrls: ['./export-csv.component.scss']
 })
+
 export class ExportCsvComponent implements OnInit {
 
   constructor() { }
 
+  //Input must be set when the component is used
+  @Input() json: any
+
   ngOnInit() {
   }
 
-  OnClickButton(e)
+  OnClickButton(e: Event)
   {
-
-
+    if(this.json == null)
+      alert('No Specified File')
+    else
+      json2csv([this.json], this.download)
   }
 
+  download(err:any, csv: any)
+  {
+    let blob = new Blob([csv], {type: "text/csv"})
+    saveAs(blob, "adminBadges.csv")
+  }
 }
