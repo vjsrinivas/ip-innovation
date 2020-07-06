7f3bb89a83943bd2d2f6d27633134c7903b26270
vtang144 <wph612@vols.utk.edu>
2020-07-06 12:21:51 -0400

    Added admin component and created table

--- innovation/src/app/app.module.ts
+++ innovation/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
