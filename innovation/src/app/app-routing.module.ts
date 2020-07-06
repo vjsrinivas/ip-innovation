14e8839d8aede40d1a6141f3863adfd6eaea96bd
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2020-07-06 14:42:15 -0400

    fixed route

--- innovation/src/app/app-routing.module.ts
+++ innovation/src/app/app-routing.module.ts
@@ -4,9 +4,8 @@ import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [  
-  {path: '.', component:HomeComponent},
+  {path: '', component:HomeComponent},
   {path: 'admin', component:AdminComponent}
-  
 ];
 
 @NgModule({
