6970f95bda04f35634fddc84779e93940670d7f0
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2020-07-07 17:28:46 -0400

    stickers done

--- innovation/src/app/widgets/sticker/sticker.component.ts
+++ innovation/src/app/widgets/sticker/sticker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'sticker',
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sticker.component.scss']
 })
 export class StickerComponent implements OnInit {
+  @Input()
+  srcPath: string;
 
-  constructor() { }
+  @Input()
+  description: string;
+
+  @Input()
+  name: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
