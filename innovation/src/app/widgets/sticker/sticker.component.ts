a29966ad8317451c9c7a1b35d4ea26a18ea0ff97
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2020-07-07 17:36:33 -0400

    tooltip stuff

--- innovation/src/app/widgets/sticker/sticker.component.ts
+++ innovation/src/app/widgets/sticker/sticker.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'sticker',
   templateUrl: './sticker.component.html',
-  styleUrls: ['./sticker.component.scss']
+  styleUrls: ['./sticker.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class StickerComponent implements OnInit {
   @Input()
